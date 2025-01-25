f9dba9d9280cf1c9913509024c06c6db74355c9a
ArmanGrigorian <armangrigorian.work@gmail.com>
2025-01-25 18:09:34 +0400

    feat: added route access controller

---
+++ src/app/(frontend)/my-route/route.ts
@@ -0,0 +1,28 @@
+import configPromise from '@payload-config'
+import { headers as getHeaders } from 'next/headers'
+import { getPayload } from 'payload'
+
+export const GET = async () => {
+  const payload = await getPayload({
+    config: configPromise,
+  })
+
+  const headers = await getHeaders()
+  const { user } = await payload.auth({ headers })
+
+  if (user) {
+    const cars = await payload.find({
+      collection: 'cars',
+      select: {
+        title: true,
+      },
+    })
+
+    return Response.json({
+      cars,
+      user,
+    })
+  }
+
+  return Response.json({ message: 'Unauthorized' }, { status: 401 })
+}
